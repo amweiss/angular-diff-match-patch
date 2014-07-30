18b5eed4b818f9bd0f0ca2a771c35610203c9b7b
Adam Weiss <aweiss@synacor.com>
2014-07-30 18:41:53 -0400

    Add directives

--- angular-diff-match-patch.js
+++ angular-diff-match-patch.js
@@ -77,56 +77,132 @@ NgDmpNamespace.helpers = {
 			}
 		}
 		return html.join('');
+	},
+
+	createDiffHtml: function(left, right) {
+		if (left && right) {
+			var dmp = new diff_match_patch();
+			var diffs = dmp.diff_main(left, right);
+			return this.createHtmlFromDiffs(diffs, this.displayType.INSDEL);
+		} else {
+			return '';
+		}
+	},
+
+	createProcessingDiffHtml: function(left, right) {
+		if (left && right) {
+			var dmp = new diff_match_patch();
+			var diffs = dmp.diff_main(left, right);
+			//dmp.Diff_EditCost = 4;
+			dmp.diff_cleanupEfficiency(diffs);
+			return this.createHtmlFromDiffs(diffs, this.displayType.INSDEL);
+		} else {
+			return '';
+		}
+	},
+
+	createSemanticDiffHtml: function(left, right) {
+		if (left && right) {
+			var dmp = new diff_match_patch();
+			var diffs = dmp.diff_main(left, right);
+			dmp.diff_cleanupSemantic(diffs);
+			return this.createHtmlFromDiffs(diffs, this.displayType.INSDEL);
+		} else {
+			return '';
+		}
+	},
+
+	createLineDiffHtml: function(left, right) {
+		if (left && right) {
+			var dmp = new diff_match_patch();
+			var a = dmp.diff_linesToChars_(left, right);
+			var diffs = dmp.diff_main(a.chars1, a.chars2, false);
+			dmp.diff_charsToLines_(diffs, a.lineArray);
+			return this.createHtmlFromDiffs(diffs, this.displayType.LINEDIFF);
+		} else {
+			return '';
+		}
 	}
 };
 
 angular.module('diff-match-patch', [])
 	.filter('diff', function() {
 		return function(left, right) {
-			if (left && right) {
-				var dmp = new diff_match_patch();
-				var diffs = dmp.diff_main(left, right);
-				return NgDmpNamespace.helpers.createHtmlFromDiffs(diffs, NgDmpNamespace.helpers.displayType.INSDEL);
-			} else {
-				return '';
-			}
-		};
+			return NgDmpNamespace.helpers.createDiffHtml(left, right);
+		}
 	})
 	.filter('processingDiff', function() {
 		return function(left, right) {
-			if (left && right) {
-				var dmp = new diff_match_patch();
-				var diffs = dmp.diff_main(left, right);
-				//dmp.Diff_EditCost = 4;
-				dmp.diff_cleanupEfficiency(diffs);
-				return NgDmpNamespace.helpers.createHtmlFromDiffs(diffs, NgDmpNamespace.helpers.displayType.INSDEL);
-			} else {
-				return '';
-			}
+			return NgDmpNamespace.helpers.createProcessingDiffHtml(left, right);
 		};
 	})
 	.filter('semanticDiff', function() {
 		return function(left, right) {
-			if (left && right) {
-				var dmp = new diff_match_patch();
-				var diffs = dmp.diff_main(left, right);
-				dmp.diff_cleanupSemantic(diffs);
-				return NgDmpNamespace.helpers.createHtmlFromDiffs(diffs, NgDmpNamespace.helpers.displayType.INSDEL);
-			} else {
-				return '';
-			}
+			return NgDmpNamespace.helpers.createSemanticDiffHtml(left, right);
 		};
 	})
 	.filter('lineDiff', function() {
 		return function(left, right) {
-			if (left && right) {
-				var dmp = new diff_match_patch();
-				var a = dmp.diff_linesToChars_(left, right);
-				var diffs = dmp.diff_main(a.chars1, a.chars2, false);
-				dmp.diff_charsToLines_(diffs, a.lineArray);
-				return NgDmpNamespace.helpers.createHtmlFromDiffs(diffs, NgDmpNamespace.helpers.displayType.LINEDIFF);
-			} else {
-				return '';
-			}
+			return NgDmpNamespace.helpers.createLineDiffHtml(left, right);
 		};
-	});
+	})
+	.directive('diff', ['$compile', function factory($compile) {
+		var ddo = {
+				scope: {
+						left: '=leftObj',
+						right: '=rightObj'
+				},
+				link: function postLink(scope, iElement, iAttrs) {
+						scope.$watchGroup(['left', 'right'], function() {
+							iElement.html(NgDmpNamespace.helpers.createDiffHtml(scope.left, scope.right));
+							$compile(iElement.contents())(scope);
+						});
+				}
+		};
+		return ddo;
+	}])
+	.directive('processingDiff', ['$compile', function factory($compile) {
+		var ddo = {
+				scope: {
+						left: '=leftObj',
+						right: '=rightObj'
+				},
+				link: function postLink(scope, iElement, iAttrs) {
+						scope.$watchGroup(['left', 'right'], function() {
+							iElement.html(NgDmpNamespace.helpers.createProcessingDiffHtml(scope.left, scope.right));
+							$compile(iElement.contents())(scope);
+						});
+				}
+		};
+		return ddo;
+	}])
+	.directive('semanticDiff', ['$compile', function factory($compile) {
+		var ddo = {
+				scope: {
+						left: '=leftObj',
+						right: '=rightObj'
+				},
+				link: function postLink(scope, iElement, iAttrs) {
+						scope.$watchGroup(['left', 'right'], function() {
+							iElement.html(NgDmpNamespace.helpers.createSemanticDiffHtml(scope.left, scope.right));
+							$compile(iElement.contents())(scope);
+						});
+				}
+		};
+		return ddo;
+	}])
+	.directive('lineDiff', ['$compile', function factory($compile) {
+		var ddo = {
+				scope: {
+						left: '=leftObj',
+						right: '=rightObj'
+				},
+				link: function postLink(scope, iElement, iAttrs) {
+						scope.$watchGroup(['left', 'right'], function() {
+							iElement.html(NgDmpNamespace.helpers.createLineDiffHtml(scope.left, scope.right));
+							$compile(iElement.contents())(scope);
+						});
+				}
+		};
+		return ddo;
+	}]);
