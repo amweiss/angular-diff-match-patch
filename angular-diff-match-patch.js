95c5ef3f47f454544dee8d386478971a016bb1f4
yaacovCR <yaacovCR@gmail.com>
2018-03-22 08:30:11 -0400

    [lint] remove unused var

--- angular-diff-match-patch.js
+++ angular-diff-match-patch.js
@@ -9,7 +9,6 @@ angular.module('diff-match-patch', [])
 
 		var DIFF_INSERT = DiffMatchPatch.DIFF_INSERT;
 		var DIFF_DELETE = DiffMatchPatch.DIFF_DELETE;
-		var DIFF_EQUAL = DiffMatchPatch.DIFF_EQUAL;
 
 		var displayType = {
 			INSDEL: 0,
